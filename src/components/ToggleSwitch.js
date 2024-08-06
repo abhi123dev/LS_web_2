11df9738aebce79269eaf46ac625d28c975876d8
Abhijeet Dewanda <abhijeetdewanda111@gmail.com>
2024-08-06 20:37:32 +0530

    Update ToggleSwitch.js

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -28,27 +28,3 @@ function ToggleSwitch({ darkMode, setDarkMode }) {
 }
 
 export default ToggleSwitch;
-
-
-// import React from 'react';
-
-// function ToggleSwitch({ darkMode, setDarkMode }) {
-//   const handleToggle = () => {
-//     setDarkMode(!darkMode);
-//   };
-
-//   return (
-//     <div className="form-group form-check text-right border mt-4" style={{ backgroundColor:"orange"}}>
-//       <input
-//         type="checkbox"
-//         className="form-check-input"
-//         id="darkModeToggle"
-//         checked={darkMode}
-//         onChange={handleToggle}
-//       />
-//       <label className="form-check-label" htmlFor="darkModeToggle">Dark Mode</label>
-//     </div>
-//   );
-// }
-
-// export default ToggleSwitch;
